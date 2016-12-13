54356cd65c8da545019c5e045431c2c002cdd97e
Sabrina Bastian <hp-pc@hppc-HP-240-G3-Notebook-PC>
2016-12-13 12:11:37 +0530

    testimonial new added

--- src/main/webapp/js/states/corporate_site.js
+++ src/main/webapp/js/states/corporate_site.js
@@ -42,7 +42,8 @@ angular.module("safedeals.states.corporate_site", [])
                     })
                     .state('corporate_site.career', {
                         'url': '/career',
-                        'templateUrl': templateRoot + '/corporate_site/career.html'
+                        'templateUrl': templateRoot + '/corporate_site/career.html',
+                        'controller': 'CareerController'
                     });
         })
 
@@ -60,28 +61,39 @@ angular.module("safedeals.states.corporate_site", [])
         })
 
         .controller('ServiceController', function ($scope, TestimonialService) {
-            $scope.myInterval = 2000;
+            $scope.myInterval = 3000;
             $scope.noWrapSlides = false;
             $scope.active = 0;
             $scope.slides = [
                 {
                     image: 'images/img5.jpg',
-                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing'
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
                 },
                 {
                     image: 'images/img6.jpg',
-                    text: 'Awesome photograph'
+                    name: 'Kristiana',
+                    designation: 'photographer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
                 },
                 {
                     image: 'images/img7.jpg',
-                    text: 'That is so cool'
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
                 },
                 {
                     image: 'images/img8.jpg',
-                    text: 'I love that'
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
                 }
             ];
-
 //            $scope.IsVisible = true;
 ////            $scope.IsHidden = true;
 ////            $scope.IsContent = true;
@@ -107,14 +119,10 @@ angular.module("safedeals.states.corporate_site", [])
                 map = new google.maps.Map(mapContainer, mapProp);
             };
             drawMap();
-
             var drawDynamicMap = function (newMapProp) {
                 console.log("newMapProp %O", newMapProp);
                 map = new google.maps.Map(mapContainer, newMapProp);
-
             };
-
-
             $scope.franchises = FranchiseService.findByCityId({
                 'cityId': 78
             }, function (franchises) {
@@ -125,11 +133,8 @@ angular.module("safedeals.states.corporate_site", [])
                     }, function (locationObject) {
                         drawMarker({lat: locationObject.latitude, lng: locationObject.longitude}, franchise.name, map);
                     });
-
                 });
             });
-
-
             var drawMarker = function (position, title, map) {
                 console.log("Position :%O", position);
                 new google.maps.Marker({
@@ -139,7 +144,6 @@ angular.module("safedeals.states.corporate_site", [])
                             // icon: 'images/icons_svg/dot.png'
                 });
             };
-
             $scope.setCity = function (city) {
                 $scope.city = city;
                 $scope.franchises = FranchiseService.findByCityId({
@@ -162,15 +166,12 @@ angular.module("safedeals.states.corporate_site", [])
                     });
                 });
             };
-
             $scope.searchCities = function (searchTerm) {
                 console.log("Search Term :%O", searchTerm);
                 return CityService.findByNameLike({
                     'name': searchTerm
                 }).$promise;
             };
-
-
 //            if (
 //                    $stateParams.offset === undefined ||
 //                    isNaN($stateParams.offset) ||
@@ -202,7 +203,6 @@ angular.module("safedeals.states.corporate_site", [])
 //                });
 //            });
             console.log("$scope.franchises", $scope.franchises);
-
             $scope.nextPage = function () {
                 $scope.currentOffset += paginationLimit;
                 $state.go(".", {'offset': $scope.currentOffset}, {'reload': true});
@@ -228,22 +228,21 @@ angular.module("safedeals.states.corporate_site", [])
             $scope.sendSms = function (clientNumber) {
                 EnquiryService.sendSms();
             };
-
             $scope.$watch('editableEnquiry.category', function (category) {
                 console.log("Category :" + category);
-                if(category === "PROPERTY_GUIDANCE"){
+                if (category === "PROPERTY_GUIDANCE") {
                     $scope.propertyGuidance = true;
                     $scope.propertySelling = false;
                     $scope.propertyBuying = false;
-                }else if(category === "PROPERTY_SELLING"){
+                } else if (category === "PROPERTY_SELLING") {
                     $scope.propertyGuidance = false;
                     $scope.propertySelling = true;
                     $scope.propertyBuying = false;
-                }else if(category === "PROPERTY_BUYING"){
+                } else if (category === "PROPERTY_BUYING") {
                     $scope.propertyGuidance = false;
                     $scope.propertySelling = false;
                     $scope.propertyBuying = true;
-                }else{
+                } else {
                     $scope.propertyGuidance = false;
                     $scope.propertySelling = false;
                     $scope.propertyBuying = false;
@@ -254,7 +253,6 @@ angular.module("safedeals.states.corporate_site", [])
             console.log("EventController");
             $scope.events = EventService.findByDate();
             console.log("$scope.events", $scope.events);
-
             $scope.myInterval = 2000;
             $scope.noWrapSlides = false;
             $scope.active = 0;
@@ -280,7 +278,7 @@ angular.module("safedeals.states.corporate_site", [])
             $scope.slides = [
                 {
                     image: 'images/banner.jpg',
-                    text:'To take an Informed Decision'
+                    text: 'To take an Informed Decision'
                 },
                 {
                     image: 'images/banner.jpg'
@@ -289,4 +287,47 @@ angular.module("safedeals.states.corporate_site", [])
                     image: 'images/banner.jpg'
                 }
             ];
+        })
+        .controller('CareerController', function ($scope, TestimonialService) {
+            $scope.myInterval = 3000;
+            $scope.noWrapSlides = false;
+            $scope.active = 0;
+            $scope.slides = [
+                {
+                    image: 'images/img5.jpg',
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
+                },
+                {
+                    image: 'images/img6.jpg',
+                    name: 'Kristiana',
+                    designation: 'photographer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
+                },
+                {
+                    image: 'images/img7.jpg',
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
+                },
+                {
+                    image: 'images/img8.jpg',
+                    name: 'Kristiana',
+                    designation: 'Web Developer www.example1.com',
+                    text: 'Lorem ipsum dolor sit amet consectetur quam felis, ultricies nec, pellentesque eu, pretium quis, sem\n\
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec. In enim justo,rhoncus ut'
+                }
+            ];
+//            $scope.IsVisible = true;
+////            $scope.IsHidden = true;
+////            $scope.IsContent = true;
+////            $scope.IsHide = true;
+////            $scope.IsToggle = true;
+//            $scope.testimonials = TestimonialService.query();
+//            console.log("$scope.testimonials", $scope.testimonials);
+//            $scope.myInterval = 3000;
         });
