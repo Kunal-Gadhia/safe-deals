7ff36aaf5b63fe21ac33265c23a286c1c9e1f9d4
hp <swapnika.godbole@vsquaresystem.com>
2017-01-16 09:42:42 +0530

    property master commit

--- src/main/java/com/vsquaresystem/safedeals/project/ProjectDAL.java
+++ src/main/java/com/vsquaresystem/safedeals/project/ProjectDAL.java
@@ -13,7 +13,6 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
