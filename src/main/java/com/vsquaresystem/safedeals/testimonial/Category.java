da8a63f01ebcc8f636974869bc961cb17344ce98
hp <swapnika.godbole@vsquaresystem.com>
2017-06-05 10:22:05 +0530

    testimonial master

---
+++ src/main/java/com/vsquaresystem/safedeals/testimonial/Category.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vsquaresystem.safedeals.testimonial;
+
+/**
+ *
+ * @author hp
+ */
+public enum Category {
+
+    MEMBERS,
+    BUYERS,
+    INDIVIDUAL_SELLERS,
+    EXPERTS_VIEW
+}
