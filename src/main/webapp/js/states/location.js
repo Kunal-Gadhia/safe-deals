863553ea6531cbaced46cad816d0cfc3d73c0e06
kunal_gadhia <kunal.gadhia@vsquaresystem.com>
2017-06-27 12:20:52 +0530

    Added manual distance calculator & fetched all workplaces & amenities within 5 Kms

--- src/main/webapp/js/states/location.js
+++ src/main/webapp/js/states/location.js
@@ -358,30 +358,32 @@ angular.module("safedeals.states.location", [])
             var drawAmenityMarker = function (position, title, location, map) {
                 console.log("Position :%O", position);
                 console.log("Map :%O", map);
-                var markers = [];
-                function setMapOnAll(map) {
-                    for (var i = 0; i < markers.length; i++) {
-                        markers[i].setMap(map);
-                    }
-                }
-//                setMapOnAll(null);
-                console.log("What Happenned, are markers cleared");
+                /////////////////Marker Clearance///////////////////
+//                var markers = [];
+//                function setMapOnAll(map) {
+//                    for (var i = 0; i < markers.length; i++) {
+//                        markers[i].setMap(map);
+//                    }
+//                }
+////                setMapOnAll(null);
+//                console.log("What Happenned, are markers cleared");
                 var marker = new google.maps.Marker({
                     map: map,
                     position: position,
-                    title: title,
+                    title: title
 //                    icon: 'images/icons_svg/airport.png'
                 });
-                markers.push(marker);
-                console.log("Markers Array :%O", markers);
-                function setMapOnAll(map) {
-                    console.log("Setting map null :%O", map);
-                    for (var i = 0; i < markers.length; i++) {
-                        markers[i].setMap(map);
-                    }
-                }
-
-                setMapOnAll(null);
+//                markers.push(marker);
+//                console.log("Markers Array :%O", markers);
+//                function setMapOnAll(map) {
+//                    console.log("Setting map null :%O", map);
+//                    for (var i = 0; i < markers.length; i++) {
+//                        markers[i].setMap(map);
+//                    }
+//                }
+//
+//                setMapOnAll(null);
+//////////////////////////////////////////////////////////////////////////
 
                 google.maps.event.addListener(marker, 'click', function () {
                     $scope.infowindow.setContent(title);
@@ -439,9 +441,9 @@ angular.module("safedeals.states.location", [])
             };
 
             $scope.createAmenityMarker = function (place, location, map) {
-                console.log("Place in marker :%O", place);
-                console.log("Location :%O", location);
-                console.log("Map :%O", map);
+                console.log("Place in marker KG:%O", place);
+                console.log("Location KG:%O", location);
+                console.log("Map KG:%O", map);
                 var placeLoc = place.geometry.location;
                 var marker = new google.maps.Marker({
                     map: map,
@@ -562,6 +564,30 @@ angular.module("safedeals.states.location", [])
                 myCity.setMap($scope.map);
                 $scope.map.fitBounds(myCity.getBounds());
             });
+
+            //////////////////////Distance Calculator Manual////////////////////
+            $scope.getDistanceFromLatLonInKm = function (lat1, lon1, lat2, lon2) {
+                console.log("Lat 1 :%O", lat1);
+                console.log("Lat 2 :%O", lat2);
+                console.log("Long 1 :%O", lon1);
+                console.log("Long 2 :%O", lon2);
+                var R = 6371; // Radius of the earth in km
+                var dLat = $scope.deg2rad(lat2 - lat1);  // deg2rad below
+                var dLon = $scope.deg2rad(lon2 - lon1);
+                var a =
+                        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.cos($scope.deg2rad(lat1)) * Math.cos($scope.deg2rad(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+                        ;
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = R * c; // Distance in km
+                return d;
+            };
+
+            $scope.deg2rad = function (deg) {
+                return deg * (Math.PI / 180);
+            };
+            ////////////////////////////////////////////////////////////////////
             $scope.getAmenityDetailByAmenity = function (amenityDetail) {
                 console.log("Final amenity :%O", amenityDetail);
                 console.log("Location in final :%O", $scope.location);
@@ -835,8 +861,16 @@ angular.module("safedeals.states.location", [])
                     'amenityId': amenityDetail.id,
                     'cityId': $scope.location.cityId
                 }, function (amenityDetailObject) {
-                    $scope.amenityDetailsList = amenityDetailObject;
+                    $scope.amenityDetailsList = [];
                     angular.forEach(amenityDetailObject, function (amenityDetail) {
+                        var d = $scope.getDistanceFromLatLonInKm($scope.location.latitude, $scope.location.longitude, amenityDetail.latitude, amenityDetail.longitude);
+                        if (d <= "5") {
+                            $scope.amenityDetailsList.push(amenityDetail);
+                        }
+                    });
+
+//                    $scope.amenityDetailsList = amenityDetailObject;
+                    angular.forEach($scope.amenityDetailsList, function (amenityDetail) {
                         drawAmenityMarker({lat: amenityDetail.latitude, lng: amenityDetail.longitude}, amenityDetail.name, $scope.location, $scope.map);
 //                        var infoWindow = new google.maps.InfoWindow({
 //                            'content': amenityDetail.name
@@ -856,8 +890,14 @@ angular.module("safedeals.states.location", [])
                     'amenityId': amenityDetail.id,
                     'cityId': $scope.location.cityId
                 }, function (amenityDetailObject) {
-                    $scope.amenityDetailsList = amenityDetailObject;
+//                    $scope.amenityDetailsList = amenityDetailObject;
                     angular.forEach(amenityDetailObject, function (amenityDetail) {
+                        var d = $scope.getDistanceFromLatLonInKm($scope.location.latitude, $scope.location.longitude, amenityDetail.latitude, amenityDetail.longitude);
+                        if (d <= "5") {
+                            $scope.amenityDetailsList.push(amenityDetail);
+                        }
+                    });
+                    angular.forEach($scope.amenityDetailsList, function (amenityDetail) {
                         drawWorkplaceMarker({lat: amenityDetail.latitude, lng: amenityDetail.longitude}, amenityDetail.name, $scope.location, $scope.map1);
 //                        var amenityCoordinates = new google.maps.LatLng(amenityDetail.latitude, amenityDetail.longitude);
 //                        var workplaceMarker = new google.maps.Marker({
