bbca529608db3f4653d75d947d54648a3f67358e
hp-pc <hp-pc@hppc-HP-240-G3-Notebook-PC>
2017-08-22 11:32:29 +0530

    unit changes in project and location master

--- src/main/webapp/js/states/project_master.js
+++ src/main/webapp/js/states/project_master.js
@@ -349,7 +349,7 @@ angular.module("safedeals.states.project_master", ['ngComboDatePicker'])
             };
             $scope.setUnit = function (unit) {
                 $scope.editableProject.unitObject = unit;
-                $scope.editableProject.unit = unit.id;
+                $scope.editableProject.unit = unit.abbreviation;
             };
             $scope.searchUnit = function (searchTerm) {
                 return UnitService.findByNameLike({
